1d8b7141616fdc5800e04301cde6dad797593646
alluesma <alluesma@stratio.com>
2019-01-10 08:53:47 +0100

    BPBD-270: Performance test of 36000 rows to return

--- src/metabase/query_processor.clj
+++ src/metabase/query_processor.clj
@@ -296,7 +296,7 @@
 
 (def ^:private ^:const max-results
   "General maximum number of rows to return from an API query."
-  10000)
+  36000)
 
 (def default-query-constraints
   "Default map of constraints that we apply on dataset queries executed by the api."
