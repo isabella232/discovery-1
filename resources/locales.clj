b78a83dcdd47df0232b13e1e943600185c6bb7b7
Marcello Leida <32846138+mleida-stratio@users.noreply.github.com>
2020-02-05 11:07:41 +0100

    Revert "[ROCK-1297] Added Oracle jdbc driver for Sanitas project (#177)"
    This reverts commit 7075221df312901394de67780f948bbce71e6237.

--- resources/locales.clj
+++ resources/locales.clj
@@ -1,5 +1,5 @@
 {
-  :locales  #{"en" "ca" "nb" "pt" "it" "vi" "fr" "ru" "es" "tr" "ja" "nl" "uk" "pl" "de" }
+  :locales  #{"en" "fr" "nl" "it" "de" "tr" "es" "ca" "ja" "uk" "ru" "pt" "vi" "nb" "pl" }
   :packages ["metabase"]
   :bundle   "metabase.Messages"
 }
